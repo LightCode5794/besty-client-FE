d3f4a1c1d43536a9e28eeb97531aad353bd9a09b
LightCode5794 <90496996+LightCode5794@users.noreply.github.com>
2023-12-04 09:52:30 +0700

    fix bug

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,4 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import counterReducer from './features/counter/counterSlice'
 import cartReducer from './features/cart/cartSlice'
 import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
